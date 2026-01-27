5c7ec3f25c2d2737bd94ffea4109529aaa29e8a7
tonysam71 <linkanverman79@gmail.com>
2026-01-27 17:19:53 +0530

    New

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,44 @@
 const express = require("express");
-const dotenv =require("dotenv")
-dotenv.config()
 const connectDB = require("./Configs/db");
 const path = require("path");
 const cors = require("cors");
 
 const app = express();
-connectDB()
 
+const PORT = process.env.PORT || 4000;
 
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }));
+connectDB();
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/uploads", express.static(path.join(__dirname, "Uploads")));
-
-
 
 const movieUserRoute = require("./Routes/movieuserRoute");
 const movieRoute = require("./Routes/MovieRoute");
 const theatreRoute = require("./Routes/TheatreRoute");
-const showRoute = require("./Routes/showRoute")
+const showRoute = require("./Routes/showRoute");
 
-app.use("/api/user", movieUserRoute); 
-app.use("/api/movie", movieRoute); 
+app.use("/api/user", movieUserRoute);
+app.use("/api/movie", movieRoute);
 app.use("/api/theatre", theatreRoute);
-app.use("/api/showmovie", showRoute)
+app.use("/api/showmovie", showRoute);
 
+const frontendPath = path.join(__dirname, "../frontend/dist");
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.use(express.static(frontendPath));
 
-
-app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
-
-const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
