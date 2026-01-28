4551287a4d65f617dc9608b3f92f08c7089c1cee
tonysam71 <linkanverman79@gmail.com>
2026-01-28 17:11:26 +0530

    New

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,21 @@
-const connectDB = require("./Configs/db");
 const dotenv = require("dotenv");
 dotenv.config();
+
 const express = require("express");
+const connectDB = require("./Configs/db");
 const path = require("path");
 const cors = require("cors");
 
 const app = express();
 connectDB();
 
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }));
-
+app.use(cors()); // ✅ CORS enabled
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use("/uploads", express.static(path.join(__dirname, "Uploads")));
+
 
 const movieUserRoute = require("./Routes/movieuserRoute");
 const movieRoute = require("./Routes/MovieRoute");
@@ -28,17 +27,22 @@ app.use("/api/movie", movieRoute);
 app.use("/api/theatre", theatreRoute);
 app.use("/api/showmovie", showRoute);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-});
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "../frontend/dist/index.html")
+  );
+});
+
+
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
