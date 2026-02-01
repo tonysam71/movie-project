976e8732e7b13ba63ed2c26b0730235316aac381
tonysam71 <linkanverman79@gmail.com>
2026-02-01 23:28:41 +0530

    Fix

--- frontend/src/Components/Profilemodal.jsx
+++ frontend/src/Components/Profilemodal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { X } from "lucide-react";
 
 export default function ProfileModal({ show, onClose, setUser }) {
